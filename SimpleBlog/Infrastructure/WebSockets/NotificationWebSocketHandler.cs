2ff2430351795b189266a57b5314121c9fe1160f
hiago <hiagoindalecio@outlook.com>
2025-05-25 16:07:53 -0300

    Limiting the number of web socket connections by IP.

--- SimpleBlog/Infrastructure/WebSockets/NotificationWebSocketHandler.cs
+++ SimpleBlog/Infrastructure/WebSockets/NotificationWebSocketHandler.cs
@@ -9,13 +9,29 @@ namespace SimpleBlog.Infrastructure.WebSockets
     public class NotificationWebSocketHandler : INotificationWebSocketHandler
     {
         private readonly ConcurrentDictionary<string, WebSocket> _sockets = new();
+        private static readonly Dictionary<string, int> _connectionsPerIp = [];
 
         public async Task HandleAsync(HttpContext context)
         {
             if (context.WebSockets.IsWebSocketRequest)
             {
-                using var socket = await context.WebSockets.AcceptWebSocketAsync();
+                var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+
+                lock (_connectionsPerIp)
+                {
+                    if (!_connectionsPerIp.ContainsKey(ip))
+                        _connectionsPerIp[ip] = 0;
+
+                    _connectionsPerIp[ip]++;
+                    if (_connectionsPerIp[ip] > 5) // Max 5 per IP
+                    {
+                        context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                        return;
+                    }
+                }
+
                 string socketId = Guid.NewGuid().ToString();
+                using var socket = await context.WebSockets.AcceptWebSocketAsync();
                 _sockets.TryAdd(socketId, socket);
 
                 while (socket.State == WebSocketState.Open)
@@ -42,6 +58,11 @@ namespace SimpleBlog.Infrastructure.WebSockets
 
                 _sockets.TryRemove(socketId, out _);
                 await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed", CancellationToken.None);
+
+                lock (_connectionsPerIp)
+                {
+                    _connectionsPerIp[ip]--;
+                }
             }
             else
             {
